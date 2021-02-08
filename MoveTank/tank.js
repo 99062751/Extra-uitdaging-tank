c154de7709205659c16d86113ba9dc998abad779
99062751 <70890704+99062751@users.noreply.github.com>
2021-02-08 14:39:42 +0100

    update

--- MoveTank/tank.js
+++ MoveTank/tank.js
@@ -1,13 +1,10 @@
 var image = document.getElementById("image");
-var bulletimg = document.getElementById("image");
-
-
-
 var positieX= parseInt(window.getComputedStyle(image).backgroundPositionX);
 var positieY= parseInt(window.getComputedStyle(image).backgroundPositionY);
+var divbullet= document.getElementById("bullet");;
 
-var bulletX= bulletimg.style.backgroundPositionX= 336;
-var bulletY= bulletimg.style.backgroundPositionY= 170;
+var divbulletX=  336;
+var divbulletY=  170;
 
 var numberRight= 84;
 var numberRight2= 5;
@@ -16,24 +13,34 @@ var numberLeft2= 5;
 var numberBottom= 5;
 var numberTop= 5;
 
+var traveldistance= 10;
+
 //de tank kan nu alleen rechts
 
 document.onkeydown = checkKey;
-image.style.transform = "rotate(90deg)"
-
+image.style.transform = "rotate(90deg)";
 function checkKey(e) {
 	console.log("key nr = " + e.keyCode);
     e = e || window.event;
     if(e.keyCode == 32){
         console.log("spacebar");
-        image.style.backgroundPositionX= positieX+ "px";  
-        image.style.backgroundPositionY= positieY+ "px";  
+        divbullet.style.display= "inline-block";
+        for(a=0; a < 4; a++){
+            setInterval(function(){
+                divbullet.style.left= traveldistance + "px";
+                traveldistance= traveldistance+30;
+            }, 500)
+                   
+            console.log(traveldistance);
+            
+        }
+        
     } else if (e.keyCode == '38') {  // up arrow
         console.log("Up arrow");
         image.style.transform = "rotate(0deg)";
          //voor de beweging
          image.style.bottom= (numberTop - numberBottom) + "px";
-         numberTop= numberTop + 10;
+         numberTop= numberTop + 15;
 
 
          //voor de lichten
@@ -44,7 +51,7 @@ function checkKey(e) {
         image.style.transform = "rotate(180deg)";
         //voor de beweging
         image.style.bottom= (numberTop - numberBottom) + "px";
-        numberBottom= numberBottom + 10;
+        numberBottom= numberBottom + 15;
 
         //voor de lichten
         image.style.backgroundPositionX= positieX + numberRight + "px"; // check goed de rupsband
@@ -58,7 +65,7 @@ function checkKey(e) {
 
         //voor de beweging
         image.style.left= (numberRight2 - numberLeft2) + "px";
-        numberLeft2= numberLeft2 + 10;
+        numberLeft2= numberLeft2 + 15;
 
     } else if (e.keyCode == '39') {   // right arrow
         image.style.transform = "rotate(90deg)";
@@ -68,7 +75,17 @@ function checkKey(e) {
         console.log(image.style.backgroundPositionX);
         //voor de beweging
         image.style.left= (numberRight2 - numberLeft2) + "px";
-        numberRight2= numberRight2 + 10;
-
+        numberRight2= numberRight2 + 15;
     }
 }
+
+
+function Createbullet(){
+    divbullet= document.createElement("div");
+    divbullet.setAttribute('id', "bullet");
+    divbullet.style.transform = "rotate(90deg)";
+    divbullet.style.display= "none";
+    document.body.appendChild(divbullet);
+}
+
+Createbullet();
